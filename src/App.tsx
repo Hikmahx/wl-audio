638d090dbd5c0a5d0371acd9026b84761ddc0f4d
Hikmahx <hikmayousuph@gmail.com>
2023-06-05 18:02:38 +0100

    set up

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+import MyRoutes from "./routes/MyRoutes";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="">
+        <MyRoutes />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'tw-cen-mt': ["'Tw Cen MT'", 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
