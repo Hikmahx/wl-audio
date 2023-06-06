2f24535d3c21a03d23414b3ea7502c0696dd60d6
Hikmahx <hikmayousuph@gmail.com>
2023-06-06 14:08:01 +0100

    style updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,9 +5,9 @@ import AudioPlayer from "../components/Home/AudioPlayer";
 
 const Home = () => {
   return (
-    <div className="mx-8 flex items-center lg:items-start h-full">
+    <div className="mx-8 flex items-center h-full">
       <HeroBg />
-      <div className="relative z-30 w-full lg:w-[calc(100%_-_80px)] lg:ml-14 lg:mt-16 text-white flex flex-col items-center lg:items-start text-center lg:text-left lg:px-14 max-w-2xl lg:max-w-lg lg:mr-auto md:mx-auto">
+      <div className="relative z-30 w-full lg:w-[calc(100%_-_80px)] lg:ml-14 xl:ml-60 lg:mt-0 text-white flex flex-col items-center lg:items-start text-center lg:text-left lg:px-14 max-w-2xl lg:max-w-lg lg:mr-auto md:mx-auto">
         <h1 className="text-6xl mb-2">NEW SINGLE</h1>
         <h2 className="text-lg tracking-[0.2em] mb-7">
           OUT NOW ON ALL PLATFORMS
